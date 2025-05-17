df37a4459162524170084ba175d41c99bb3230a8
ghapzh <gabriel.alejandro.paparello@gmail.com>
2025-05-17 18:12:03 +0200

    hero mobile

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import { nunito } from "../lib/fonts/fonts";
@@ -5,25 +6,26 @@ import Link from "next/link";
 
 export const Hero = () => {
   return (
-    <div className="flex h-screen w-screen relative">
-      {/* Texto sobre las imágenes */}
+    <div className="flex flex-col sm:flex-row h-screen w-screen relative">
+      {/* Títulos */}
       <h1
-        className={`${nunito.className} absolute z-30 top-[122px] left-1/2 -translate-x-1/2  text-[67.73px]  tracking-[13.5px] text-[#EBEBF5] font-semibold`}
+        className={`${nunito.className} absolute z-30 top-[50px] sm:top-[122px] left-1/2 -translate-x-1/2 text-[36px] sm:text-[67.73px] tracking-[5px] sm:tracking-[13.5px] text-[#EBEBF5] font-semibold text-center`}
       >
         LAURA’S BAKERY
       </h1>
+
       <h3
-        className={`${nunito.className} absolute z-30 top-[200px] left-1/2 -translate-x-1/2 font-extralight pl-70 text-[41.88px] tracking-[5.2px] text-[#EBEBF5] 
-        `}
+        className={`${nunito.className} absolute z-30 top-[150px] sm:top-[200px] left-1/2 -translate-x-1/2 text-[24px] sm:text-[41.88px] tracking-[3px] sm:tracking-[5.2px] text-[#EBEBF5] font-extralight text-center`}
       >
         CRAFTS
       </h3>
 
-      {/* Rectángulo de altura completa en el centro */}
-      <div className="absolute top-0 bottom-0 left-1/2 -translate-x-1/2 w-[45px] h-full bg-[#111313] z-10"></div>
+      {/* Rectángulo vertical central */}
+      <div className="hidden sm:block absolute top-0 bottom-0 left-1/2 -translate-x-1/2 w-[45px] h-full bg-[#111313] z-10"></div>
+      <div className="sm:hidden absolute left-0 right-0 top-1/2 -translate-y-1/2 h-[25px] w-full bg-[#111313] z-10"></div>
 
       {/* Imagen 1 */}
-      <div className="w-1/2 h-full relative">
+      <div className="relative w-full sm:w-1/2 h-1/2 sm:h-full order-2 sm:order-1">
         <Image
           src="/hero1.jpg"
           alt="Hero Image 1"
@@ -31,12 +33,10 @@ export const Hero = () => {
           className="object-cover"
         />
         <div className="absolute inset-0 bg-[#111313]/30 z-10"></div>
-        <button
-          className={`z-20 absolute bottom-[25%]  left-1/2 -translate-x-1/2 p-4 bg-[#271F15]`}
-        >
+        <button className="z-20 absolute bottom-[15%] left-1/2 -translate-x-1/2 p-3 sm:p-4 bg-[#271F15]">
           <Link
             href="/productos"
-            className={` {${nunito.className}  text-center text-white text-[16px] tracking-[5.2px] pl-1 font-semibold`}
+            className={`${nunito.className} text-center text-white text-[14px] sm:text-[16px] tracking-[3px] sm:tracking-[5.2px] font-semibold`}
           >
             PRODUCTOS
           </Link>
@@ -44,7 +44,7 @@ export const Hero = () => {
       </div>
 
       {/* Imagen 2 */}
-      <div className="w-1/2 h-full relative">
+      <div className="relative w-full sm:w-1/2 h-1/2 sm:h-full  order-1 sm:order-2">
         <Image
           src="/hero2.jpg"
           alt="Hero Image 2"
@@ -52,14 +52,12 @@ export const Hero = () => {
           className="object-cover"
         />
         <div className="absolute inset-0 bg-[#111313]/30 z-10"></div>
-        <button
-          className={`z-20 absolute bottom-[25%]  left-1/2 -translate-x-1/2 p-4 bg-[#47231C]`}
-        >
+        <button className="z-20 absolute bottom-[15%] left-1/2 -translate-x-1/2  sm:p-4 bg-[#47231C]">
           <Link
             href="/productos"
-            className={` {${nunito.className}  text-center text-white text-[16px] tracking-[5.2px] pl-1 font-semibold`}
+            className={`${nunito.className} text-center text-white text-[14px] sm:text-[16px] tracking-[3px] sm:tracking-[5.2px] font-semibold`}
           >
-            ¿DONDE ESTAMOS?
+            ¿DÓNDE ESTAMOS?
           </Link>
         </button>
       </div>
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { nunito } from "../lib/fonts/fonts";
 
 export const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full  z-20 px-20 py-2 bg-[#111313]/20">
+    <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full z-20 px-20 py-2 bg-[#111313]/20">
+      {/* Botón hamburguesa en mobile */}
+      <div className="sm:hidden flex justify-end">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex flex-col justify-between w-6 h-5"
+          aria-label="Toggle menu"
+        >
+          <span className="block h-0.5 bg-white"></span>
+          <span className="block h-0.5 bg-white"></span>
+          <span className="block h-0.5 bg-white"></span>
+        </button>
+      </div>
+
+      {/* Menú */}
       <ul
-        className={`flex justify-between ${nunito.className} text-white p-4 text-[16px] tracking-[5.2px] font-semibold`}
+        className={`
+          ${
+            nunito.className
+          } text-white p-4 text-[16px] tracking-[5.2px] font-semibold
+          ${
+            isOpen ? "flex" : "hidden"
+          } flex-col sm:flex sm:flex-row sm:justify-between
+        `}
       >
         <li>MENUS</li>
         <li>RESERVATION</li>
