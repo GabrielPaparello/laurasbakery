2c560b6143252bb1bcc4b5111511f79187322b41
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2025-05-17 19:28:05 +0200

    Update fonts.tsx

--- app/lib/fonts/fonts.tsx
+++ app/lib/fonts/fonts.tsx
@@ -4,6 +4,6 @@ import { Nunito, Tinos } from "next/font/google";
 const nunito = Nunito({ subsets: ["latin"] });
 
 // Tinos NO acepta subsets, pero sí necesita los pesos definidos
-const tinos = Tinos({ weight: ["400", "700"] }); // puedes ajustar según lo que necesites
+const tinos = Tinos({ weight: ["400", "700"], preload:false }); // puedes ajustar según lo que necesites
 
 export { nunito, tinos };
