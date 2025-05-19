333457eb1641a0b5af3d61f4684defaa82c1169d
ghapzh <gabriel.alejandro.paparello@gmail.com>
2025-05-19 10:58:52 +0200

    d

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,14 +5,14 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear(); // Obtener el año actual dinámicamente
 
   return (
-    <div className="bg-[#FBF1E7] flex flex-col items-center justify-center py-10 sm:gap-2 gap-5">
+    <div className="bg-[#47231C] flex flex-col items-center justify-center py-10 sm:gap-2 gap-5">
       <h2
         className={`${nunito.className} text-[24px] sm:text-[41.88px] text-[#47231C]`}
       >
         VENI A CONOCERNOS
       </h2>
       <p
-        className={`${nunito.className} text-[#47231C] text-[14px] sm:text-[16px] tracking-[5.2px] font-semibold text-center w-[80%]`}
+        className={`${nunito.className} text-[#FBF1E7] text-[14px] sm:text-[16px] tracking-[5.2px] font-semibold text-center w-[80%]`}
       >
         EN LAURA’S BAKERY, CADA BOCADO ES UNA CELEBRACIÓN DE LA VIDA Y LA
         ALEGRÍA. TE ESPERAMOS CON LOS BRAZOS ABIERTOS Y UNA SONRISA EN EL
